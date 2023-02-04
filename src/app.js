05fa449037ef1cb6c79a9b2a32b33f674add8470
deontaemurphy <deontae2011@gmail.com>
2023-02-04 00:32:29 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//write your code here
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoexcuse = (document.querySelector(
+    "#excuse"
+  ).innerHTML = generateExcuse());
 };
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randWho = who[Math.floor(Math.random() * who.length)];
+  let randAction = action[Math.floor(Math.random() * action.length)];
+  let randWhat = what[Math.floor(Math.random() * what.length)];
+  let randWhen = when[Math.floor(Math.random() * when.length)];
+
+  return randWho + " " + randAction + " " + randWhat + " " + randWhen;
+}
